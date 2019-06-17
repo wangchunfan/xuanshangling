e2c4487422f04889addc7f3c497a61c81bb9f4a6
wangcf <171250669@qq.com>
2019-06-17 21:21:54 +0800

    给controller添加@Api注解

--- xuanshangling-backend/xuanshangling-api/src/main/java/club/xuanshangling/controller/HelloController.java
+++ xuanshangling-backend/xuanshangling-api/src/main/java/club/xuanshangling/controller/HelloController.java
@@ -1,5 +1,7 @@
 package club.xuanshangling.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description ${description}
  */
 @RestController
+@Api(value = "hello接口", tags = "测试controller")
 public class HelloController {
+
     @GetMapping("/hello")
-    public String hello(){
+    @ApiOperation(value = "hello", notes = "测试")
+    public String hello() {
         return "hello";
     }
 }
--- xuanshangling-backend/xuanshangling-api/src/main/java/club/xuanshangling/controller/RegisterLoginController.java
+++ xuanshangling-backend/xuanshangling-api/src/main/java/club/xuanshangling/controller/RegisterLoginController.java
@@ -3,10 +3,11 @@ package club.xuanshangling.controller;
 import club.xuanshangling.pojo.User;
 import club.xuanshangling.service.UserService;
 import club.xuanshangling.utils.JsonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +19,14 @@ import java.security.NoSuchAlgorithmException;
  */
 
 @RestController
+@Api(value = "用户登录注册接口", tags = "登录注册controller")
 public class RegisterLoginController {
 
     @Autowired
     UserService userService;
 
     @PostMapping("/register")
+    @ApiOperation(value = "用户注册",notes ="用户注册post接口")
     public JsonResult register(@RequestBody User user) throws NoSuchAlgorithmException {
         //判断账号密码不为空
         if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword()))
