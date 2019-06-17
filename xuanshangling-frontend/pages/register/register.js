6996bbd759ec64de53c567bff7a61399780778f6
wangcf <171250669@qq.com>
2019-06-17 23:10:43 +0800

    小程序端注册功能实现

---
+++ xuanshangling-frontend/pages/register/register.js
@@ -0,0 +1,37 @@
+const app = getApp();   
+Page({
+  doRegister: function (res) {
+    var data = res.detail.value
+    if(!data.username || !data.password){
+      wx.showToast({
+        title: '用户名或密码不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    console.log(app)
+    wx.request({
+      url: app.serverUrl + '/register',
+      method: 'POST',
+      data: {
+        username: data.username,
+        password: data.password
+      },
+      success: function(res){
+        if (res.data.code == 0) {
+         wx.showToast({
+           title: '注册成功'
+         })
+        } else{
+          wx.showToast({
+            title: res.data.msg,
+            icon: "none"
+          })
+        }
+      },
+      fail: function(res) {
+        console.log(res);
+      }
+    })
+  }
+})
