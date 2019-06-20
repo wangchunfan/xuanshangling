2594772c06111e1792fc733ba572bda25be8bc59
wangcf <171250669@qq.com>
2019-06-20 23:59:13 +0800

    小程序端 用户主页上传修改头像

---
+++ xuanshangling-frontend/pages/mine/mine.js
@@ -0,0 +1,73 @@
+const app = getApp();
+Page({
+  data: {
+    faceUrl: "../../images/face-default.jpg"
+  },
+  //更改头像
+  changeFace() {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      success: function(res) {
+        const tempFilePath  = res.tempFilePaths;
+        if(tempFilePath.length == 0) return;
+        wx.showLoading({
+          title: '正在上传。。。',
+        })
+        //上传
+        wx.uploadFile({
+          url: app.serverUrl + '/user/uploadFace?userId=' + app.userInfo.id,
+          filePath: tempFilePath[0],
+          name: 'file',
+          success: res => {
+            var data = JSON.parse(res.data);
+            if (data.code == 0){
+              wx.hideLoading();
+              wx.showToast({
+                title: '上传成功！',
+              })
+              that.setData({
+                faceUrl: data.data
+              })
+
+            }else {
+              wx.showToast({
+                title: data.msg,
+                icon: 'none'
+              })
+            }
+          }
+        })
+      },
+    })
+  },
+  //前往登录页面
+  doLogin() {
+    wx.redirectTo({
+      url: '../login/login',
+    })
+  },
+  //用户注销
+  doLogout() {
+    wx.request({
+      url: app.serverUrl + '/logout?userId=' + app.userInfo.id,
+      method: "POST",
+      success: res => {
+        if(res.data.code == 0) {
+          wx.showToast({
+            title: '注销成功',
+            duration: 3000
+          })
+          wx.redirectTo({
+            url: '../login/login',
+          })
+        }else {
+           wx.showToast({
+             title: res.data.msg,
+             icon: 'none'
+           })
+        }
+      }
+    })
+  }
+})
