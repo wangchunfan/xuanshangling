1d756dbd3aa1ceb46d3bad873c3746b612a7123d
wangcf <171250669@qq.com>
2019-06-22 10:17:39 +0800

    设置服务器的虚拟路径

---
+++ xuanshangling-backend/xuanshangling-api/src/main/java/club/xuanshangling/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package club.xuanshangling;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Author: wangcf
+ * @Date: 2019/6/22 9:32
+ * @Description 配置服务器虚拟路径
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("file:files/");
+    }
+
+}
