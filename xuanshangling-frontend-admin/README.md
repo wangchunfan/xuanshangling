16a3be6cbabd1e5bf9f95c29eefa622a3ca83157
言凡 <171250669@qq.com>
2019-07-20 18:47:11 +0800

    后台管理系统 前端UI框架

---
+++ xuanshangling-frontend-admin/README.md
@@ -0,0 +1,6 @@
+# 悬赏令后台管理系统-前端
+
+
+
+---
+UI框架来源：[vue-manage-system](https://github.com/lin-xin/vue-manage-system)
---
+++ xuanshangling-frontend-admin/src/components/page/Donate.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-lx-redpacket_fill"></i> 支持作者</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">
+                如果该项目对你有帮助，那就请作者喝杯饮料吧！
+            </div>
+            <div><img src="https://note.youdao.com/yws/api/personal/file/A6A938D9740049859D1DC97331B71375?method=download&shareKey=c039a2063e2cb71b1c45a302f5a19fe4"></div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
